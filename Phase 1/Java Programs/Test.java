f9c5f6a94011f8fdb6a629fe3dc7850368c44d2d
Akash <akash300383@gmail.com>
2022-05-17 17:48:33 +0530

    done

---
+++ Phase 1/Java Programs/Test.java
@@ -0,0 +1,22 @@
+//import java.util.Scanner;
+import java.util.*;
+class Test {
+	public static void main(String args[]) {
+	// while loop 
+	/*
+	int i=0,n=10;		// initiailzation 
+	while(i  <= n) {		// condition true 	// entry loop 
+		System.out.println("The value of i is "+i);
+		i++;	//i=i+1
+	}
+	*/
+	// do while loop 
+	int i=0,n=10;
+	do {
+		System.out.println("The value of i is "+i);
+		i++;
+	}while(i>=n);			// exit loop consider 
+
+	System.out.println("Finish");
+	}
+}
