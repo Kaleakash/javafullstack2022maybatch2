df5401ebeeb750942bd35810fd3410c69bcc0acc
akash <akash300383@gmail.com>
2022-09-26 15:56:20 +0530

    done

---
+++ Phase 5/My Images/Image to run the Spring boot Application/spring-boot-with-docker/spring-boot-with-docker/src/main/java/com/SpringBootWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDockerApplication.class, args);
+	}
+
+}
---
+++ Phase 5/My Images/Image to run the Spring boot Application/spring-boot-with-docker/spring-boot-with-docker/src/main/java/com/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping(value = "/")
+	public String greeting() {
+		return "Welcome to Spring boot with Docker";
+	}
+	@GetMapping(value = "say/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "Welcome user "+name+" Spring boot with Docker";
+		
+	}
+}
