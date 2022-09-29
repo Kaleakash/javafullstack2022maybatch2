5d142974996f40be5bf72daabaa409dbe7adbc2d
akash <akash300383@gmail.com>
2022-09-29 17:54:48 +0530

    done

---
+++ Phase 5/AWS with Spring Boot Projects/spring-boot-with-aws/spring-boot-with-aws/src/main/java/com/controller/SampleController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping(value = "/")
+	public String greeting() {
+		return "Welcome to Spring boot with AWS created by Akash";
+	}
+	@GetMapping(value = "say/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "Welcome user "+name+" to Spring boot with AWS";
+	}
+}
